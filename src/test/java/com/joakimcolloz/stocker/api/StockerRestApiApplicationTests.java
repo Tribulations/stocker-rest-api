9afea8631ff6600466f865f7bdf6ac172b5807df
Joakim Colloz <kung_jocke@hotmail.com>
2025-08-07 15:58:40 +0200

    Initial commit

---
+++ src/test/java/com/joakimcolloz/stocker/api/StockerRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joakimcolloz.stocker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockerRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
